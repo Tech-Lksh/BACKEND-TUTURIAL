6a681a372035c61e4525c10aab9c046602e7fb8c
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-28 07:11:19 +0000

    raect

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
-const  catMe = require('cat-me')
+{/* Beignner Level production server creation */}
+{/*
+    
+const http = require('http')
 
-console.log(catMe())
+const server = http.createServer( (req, res)=> {
+    if(req.url == "/about") {
+        res.end('The about page')
+    } 
+
+    if(req.url == "/profile") {
+        res.end('The profile page')
+    }
+
+    if(req.url == "/") {
+        res.end('The home page')
+    }
+})
+
+server.listen(3000)
+
+*/}
+
+
+{/* Production Level Server Creation */}
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=> {
+    res.render('index')
+})
+
+app.get('/about', (req, res)=> {
+    res.send('About Page')
+})
+
+app.listen(3000)
